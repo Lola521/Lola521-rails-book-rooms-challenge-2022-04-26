2ebabc3a68bfb96aa051960b1a220119bd7cf885
guole jing <guole.jing@gmail.com>
2022-04-26 19:33:13 +0200

    model validates

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,10 @@
 class Room < ApplicationRecord
   belongs_to :hotel
+  has_many :bookings, dependent: :destroy
+
+  validates :capacity, presence: true
+  validates :capacity, numericality: { only_integer: true }
+  validates_numericality_of :capacity, :greater_than_or_equal_to => 0
+  validates :price_per_night, presence: true
+  validates :price_per_night, :greater_than => 0
 end
