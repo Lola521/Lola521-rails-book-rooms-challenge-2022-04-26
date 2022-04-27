4c5dbeb9585361c9e7113f9b34fd4e4b98ffbc4e
guole jing <guole.jing@gmail.com>
2022-04-27 13:06:14 +0200

    show room

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,18 @@
+class BookingController < ApplicationController
+  def create
+    @room = Room.find(params[:room_id])
+    @booking = Booking.new(booking_params)
+    @booking.room = @room
+      if @booking.save
+        redirect_to room_path(@room)
+      else
+        render "rooms/show"
+      end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_at, :ends_at)
+  end
+end
