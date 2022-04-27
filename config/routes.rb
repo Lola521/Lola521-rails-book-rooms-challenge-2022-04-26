af192d053497b9222cc9e59d1ee5f20950830167
guole jing <guole.jing@gmail.com>
2022-04-27 09:24:29 +0200

    rooms controller view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
   devise_for :users
   root to: 'pages#home'
+  resources :rooms, only: [:index, :show] do
+    resources :booking, only: [:new, :create]
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
